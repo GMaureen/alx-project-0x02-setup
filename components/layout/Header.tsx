147c9708b3f1398c8c19c21c47839ea506772bf3
GMaureen <maureenmokgele97@gmail.com>
2025-11-22 22:08:03 +0200

    Add Header component with navigation links

---
+++ components/layout/Header.tsx
@@ -0,0 +1,20 @@
+# components/layout/Header.tsx
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-600 text-white p-4 flex justify-around items-center">
+      <Link href="/home" className="hover:underline">
+        Home
+      </Link>
+      <Link href="/about" className="hover:underline">
+        About
+      </Link>
+      <Link href="/posts" className="hover:underline">
+        Posts
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
